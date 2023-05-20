9f57ca5dea8ac07c9bb05b39d05a70c0949e0a61
MPILOGUM114 <133761097+MPILOGUM114@users.noreply.github.com>
2023-05-20 20:14:52 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MeAndyou'
+copyright = '2023, IgnitionGroup'
+author = 'IgnitionGroup'
 
 release = '0.1'
 version = '0.1.0'
